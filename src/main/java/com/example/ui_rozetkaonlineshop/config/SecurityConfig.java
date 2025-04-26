b45ce651e6c5904307d34aa48f5631bd98b92a9f
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-04-26 22:58:26 +0300

    update popular-categories.css

--- src/main/java/com/example/ui_rozetkaonlineshop/config/SecurityConfig.java
+++ src/main/java/com/example/ui_rozetkaonlineshop/config/SecurityConfig.java
@@ -1,16 +1,10 @@
 package com.example.ui_rozetkaonlineshop.config;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
